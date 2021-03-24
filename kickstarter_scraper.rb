80add709584bb7bc83696d6e71951e324fa49c61
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-24 00:03:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,15 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+
+  projects = {}
+
+  kickstarer.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  end
+
+
 end
 
 create_project_hash
